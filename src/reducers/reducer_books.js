d0d77a0d394bf6dd9b231f5b0f01288afc5bbcec
Jerry Pallath <jpallath@nyu.edu>
2018-02-28 20:51:07 -0500

    lets push

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    {title: 'Javascript: The Good Parts', pages: 100},
+    {title: 'Harry Potter', pages: 900},
+    {title: 'The Dark Tower', pages: 10},
+    {title: 'Eloquent Ruby', pages: 1}
+  ]
+}
